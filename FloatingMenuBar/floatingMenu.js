40357d28799ad376d7fd414587df09abc3bcc6ed
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-29 15:50:57 +0300

    Update floatingMenu.js

--- FloatingMenuBar/floatingMenu.js
+++ FloatingMenuBar/floatingMenu.js
@@ -8,15 +8,16 @@ jQuery(function(){
     var icon =$("i#left-right-arrow");
 
     icon.on("click",function(){
+        
         if($(this).attr("class") == "fas fa-angle-double-right")
-        {
+        {        
+            menuBar.hide("fast","linear");
             $(this).attr("class","fas fa-angle-double-left");
-            menuBar.fadeOut(300);
         }
         else
         {
+            menuBar.show("fast","linear");
             $(this).attr("class","fas fa-angle-double-right");
-            menuBar.fadeIn(300);
         }
              
     })
